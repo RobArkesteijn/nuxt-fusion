2e3e537d2c34536bf08300de93e0c08e7dfc4a35
RobArkesteijn <rarkesteijn@outlook.com>
2024-10-26 19:40:46 +0200

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'nuxt/nuxt-config-keys-order': 'off',
+    'nuxt/prefer-import-meta': 'error',
+  },
+})
